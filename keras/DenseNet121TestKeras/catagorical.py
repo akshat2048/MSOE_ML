9d74ec9f83107ca2bacbf495cf236c3c11bbdae8
Sameer Bhatt <sameer.bhatt421@gmail.com>
2022-01-26 17:53:49 -0600

    AKSHAT HELP PLS

--- keras/DenseNet121TestKeras/catagorical.py
+++ keras/DenseNet121TestKeras/catagorical.py
@@ -1,17 +1,17 @@
 from tensorflow import keras
 from tensorflow.keras.applications import DenseNet121
 import tensorflow.keras.layers as layers
-
+import numpy as np
 import environmentsettings
 
 def create_training_data_set(print_dataset=False):
     # Create a dataset
     train_dataset = keras.preprocessing.image_dataset_from_directory(
-        environmentsettings.setting_binary['TRAINING_DIRECTORY'], 
-        batch_size=environmentsettings.setting_binary['BATCH_SIZE'], 
+        environmentsettings.setting_categorical['TRAINING_DIRECTORY'], 
+        batch_size=environmentsettings.setting_categorical['BATCH_SIZE'], 
         image_size=(224, 224), 
         color_mode='rgb',
-        label_mode='binary'
+        label_mode='categorical'
     )
 
     '''
@@ -24,39 +24,17 @@ def create_training_data_set(print_dataset=False):
     '''
     label_mode: - 'int': means that the labels are encoded as integers (e.g. for sparse_categorical_crossentropy loss). - 'categorical' means that the labels are encoded as a categorical vector (e.g. for categorical_crossentropy loss). - 'binary' means that the labels (there can be only 2) are encoded as float32 scalars with values 0 or 1 (e.g. for binary_crossentropy). - None (no labels).
     '''
-
-    # For demonstration, iterate over the batches yielded by the dataset.
-    if not print_dataset:
-        return train_dataset
-    
-    for data, labels in train_dataset:
-        print(data.shape)
-        print(data.dtype)
-        print(labels.shape) 
-        print(labels.dtype)
-
     return train_dataset
 
 def create_validation_data_set(print_dataset=False):
     # Create a dataset
     test_dataset = keras.preprocessing.image_dataset_from_directory(
-        environmentsettings.setting_binary['TESTING_DIRECTORY'], 
-        batch_size=environmentsettings.setting_binary['BATCH_SIZE'], 
+        environmentsettings.setting_categorical['TESTING_DIRECTORY'], 
+        batch_size=environmentsettings.setting_categorical['BATCH_SIZE'], 
         image_size=(224, 224), 
         color_mode='rgb',
-        label_mode='binary'
+        label_mode='categorical'
     )
-
-    '''
-    image_size: Size to resize images to after they are read from disk. Defaults to (256, 256). Since the pipeline processes batches of images that must all have the same size, this must be provided.
-
-    ^^This is from the docs
-    I assume that this means that the images are resized to the image size provided
-    '''
-
-    '''
-    label_mode: - 'int': means that the labels are encoded as integers (e.g. for sparse_categorical_crossentropy loss). - 'categorical' means that the labels are encoded as a categorical vector (e.g. for categorical_crossentropy loss). - 'binary' means that the labels (there can be only 2) are encoded as float32 scalars with values 0 or 1 (e.g. for binary_crossentropy). - None (no labels).
-    '''
 
     return test_dataset
 
@@ -72,22 +50,21 @@ def create_model():
 def main():
     print("Starting")
     train_dataset = create_training_data_set()
+    # print(np.concatenate([y for x, y in train_dataset], axis = 0).shape)
     model = create_model()
-    model.layers[-1].activation = keras.activations.sigmoid
+    # model.layers[-1].activation = keras.activations.sigmoid
     # model = keras.utils.apply_modifications(model)
 
     model.compile(
-        optimizer= keras.optimizers.SGD(learning_rate=environmentsettings.setting_binary['LEARNING_RATE']),
-        loss='binary_crossentropy',
+        optimizer= keras.optimizers.SGD(learning_rate=environmentsettings.setting_categorical['LEARNING_RATE']),
+        loss='sparse_categorical_crossentropy',
         metrics=['accuracy']
     )
-    # optimizer= keras.optimizers.SGD(learning_rate=environmentsettings.setting_binary['LEARNING_RATE'])
+    # optimizer = keras.optimizers.Adam(lr=environmentsettings.setting_categorical['LEARNING_RATE'])
     # TRY ADAM WHEN YOU GET HOME, USING SGD RIGHT NOW
     history = model.fit(
         train_dataset,
-        epochs=environmentsettings.setting_binary['EPOCHS'],
-        validation_data = create_validation_data_set()
-        # batch_size = environmentsettings.setting_binary['BATCH_SIZE']
+        epochs=environmentsettings.setting_categorical['EPOCHS']
     )
 
     print(history.history)
@@ -97,6 +74,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
